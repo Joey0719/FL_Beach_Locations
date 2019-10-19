df05eecf1baa28b5507783ed69a3139ddbf29bfb
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-10-19 14:50:08 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in FL_Beach_Locations.gemspec
+gemspec
---
+++ lib/FL_Beach_Locations.rb
@@ -0,0 +1,6 @@
+require "FL_Beach_Locations/version"
+
+module FLBeachLocations
+  class Error < StandardError; end
+  # Your code goes here...
+end
