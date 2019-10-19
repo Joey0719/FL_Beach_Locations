3f08e37be2130fbf0583b87965a66fa865a6ac4c
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-10-19 16:24:32 +0000

    Framework established

--- lib/FL_Beach_Locations/cli.rb
+++ lib/FL_Beach_Locations/cli.rb
@@ -2,6 +2,7 @@ class FLBeachLocations::CLI
   
   def call
     puts "The sun is hot!"
+    scraper.get_page
   end# of call 
   
   end# of class
--- lib/FL_Beach_Locations/scraper.rb
+++ lib/FL_Beach_Locations/scraper.rb
@@ -1,3 +1,9 @@
-class Scraper 
+require 'Nokogiri'
+require 'openuri'
+require 'pry'
+
+class Scraper
   
+def self.get_page
+  html = Nokogiri::HTML(open("https://www.travelchannel.com/interests/beaches/photos/top-10-florida-beaches"))
 end# of scraper
